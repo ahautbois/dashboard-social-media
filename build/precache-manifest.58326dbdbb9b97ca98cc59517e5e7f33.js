552f71d24f330f5fc41ed00aba75cf88e2f76f0d
aurelie hautbois <aurelie.hautbois@gmail.com>
2020-05-22 15:06:15 +0200

    build folder

---
+++ build/precache-manifest.58326dbdbb9b97ca98cc59517e5e7f33.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c107b21f958a13ff87b6ba5b374e720e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "719fc7b9fda2a0416d6d",
+    "url": "/static/css/2.e9852964.chunk.css"
+  },
+  {
+    "revision": "5c998c655bc6443e9333",
+    "url": "/static/css/main.f0bd60bb.chunk.css"
+  },
+  {
+    "revision": "719fc7b9fda2a0416d6d",
+    "url": "/static/js/2.7a460321.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7a460321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c998c655bc6443e9333",
+    "url": "/static/js/main.0b6b9241.chunk.js"
+  },
+  {
+    "revision": "adcdaee554c74de3011e",
+    "url": "/static/js/runtime-main.88cf3d69.js"
+  },
+  {
+    "revision": "a738376499ac8a902fd2ce03c3030f98",
+    "url": "/static/media/icon-facebook.a7383764.svg"
+  },
+  {
+    "revision": "991487fe03dace6f69a870d3085aadf1",
+    "url": "/static/media/icon-instagram.991487fe.svg"
+  },
+  {
+    "revision": "810ccee84b0132ca7921a1b0b4ee6021",
+    "url": "/static/media/icon-twitter.810ccee8.svg"
+  },
+  {
+    "revision": "8499da129aedb70f3e20262951f7e236",
+    "url": "/static/media/icon-youtube.8499da12.svg"
+  }
+]);
